d7d7b23210d546ef9373f5032204e1c6ed1ad56e
jindrak02 <jindrak12345@seznam.cz>
2024-10-14 22:34:43 +0200

    Complete route handlers for rendering all books / notes, adding notes, db functions

--- index.js
+++ index.js
@@ -21,13 +21,51 @@ app.use(express.static("public"));
 
 // DB functions
 async function getBooks(){
-    const result = await db.query("SELECT * FROM books;");
-    return result.rows;
+
+    try {
+        const result = await db.query("SELECT * FROM books;");
+        return result.rows;
+    } catch (error) {
+        console.error("Error executing sql query for books." + error.stack);
+    }
+
 }
 
 async function getBook(id) {
-    const result = await db.query("SELECT * FROM books b WHERE b.id = $1;", [id]);
-    return result.rows[0];
+
+    try {
+        const result = await db.query("SELECT * FROM books b WHERE b.id = $1;", [id]);
+        return result.rows[0];
+    } catch (error) {
+        console.error("Error executing sql query for book." + error.stack);
+    }
+
+}
+
+async function addBook(isbn) {
+    
+}
+
+async function getNotes(bookId) {
+
+    try {
+        const result = await db.query("SELECT * FROM notes WHERE book_id = $1;", [bookId]);
+        return result.rows;
+    } catch (error) {
+        console.error("Error executing sql query for book notes." + error.stack);
+    }
+    
+}
+
+async function addNote(note, bookId) {
+
+    try {
+        const result = await db.query("INSERT INTO notes(book_id, note_text) VALUES($1, $2);", [bookId, note]);
+        console.log("Note successfully added.");
+    } catch (error) {
+        console.error("Error executing sql query for book notes." + error.stack);
+    }
+
 }
 
 // Route handlers
@@ -43,12 +81,22 @@ app.get("/add", (req, res) => {
 
 app.get("/book/:id", async (req, res) => {
     const bookId = req.params.id;
-    const book = await getBook(req.params.id);
-    res.render("book.ejs", {book: book});
+    const book = await getBook(bookId);
+    const notes = await getNotes(bookId);
+    res.render("book.ejs", {book: book, notes: notes});
 });
 
-app.get("/addNote", (req, res) => {
-    res.render("addNote.ejs");
+app.get("/addNote/book/:id", (req, res) => {
+    const bookId = req.params.id;
+    res.render("addNote.ejs", {bookId: bookId});
+});
+
+app.post("/addNote", async (req, res) => {
+    const bookId = req.body.bookId;
+    const note = req.body.note;
+    await addNote(note, bookId);
+
+    res.redirect("/book/" + bookId + "?");
 });
 
 app.listen(port, () => {
